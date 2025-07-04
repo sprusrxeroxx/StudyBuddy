9755e3289a23666c3cab2ba8e3e9c04f694f4589
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-07-04 11:14:12 +0000

    feat: add events module exports for centralized event management

---
+++ TrashMap/src/events/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Events module exports
+ * Centralizes event-related imports for the TrashMap application
+ */
+
+export { default as EventBus } from './EventBus';
+export * from './EventTypes';
