8c56c83e36510af2c35b7f29e9063686ef56e65c
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-07-04 15:04:23 +0000

    feat: add services module exports for FirebaseStorageService and LocationService

---
+++ TrashMap/src/services/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Services module exports
+ * Centralizes service-related imports for the TrashMap application
+ */
+
+export { FirebaseStorageService } from './FirebaseStorageService';
+export { LocationService } from './LocationService';
+export type { UploadResult } from './FirebaseStorageService';
+export type { LocationPermissionStatus } from './LocationService';
