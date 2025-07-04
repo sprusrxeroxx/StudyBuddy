e558b3b19bf21629e0faa62c0a489e736cabd2a1
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-07-04 15:01:31 +0000

    feat: add commands module exports for centralized command management

---
+++ TrashMap/src/commands/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Commands module exports
+ * Centralizes command-related imports for the TrashMap application
+ */
+
+export { ICommand } from './ICommand';
+export { UploadPhotoCommand } from './UploadPhotoCommand';
+export type { UploadPhotoOptions, UploadPhotoResult } from './UploadPhotoCommand';
