d5858dba12160408db492e9ec5ffb91de2173ea7
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-08-12 01:17:32 +0000

    feat: implement Home component with layout and styling

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,13 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import Logo from '../assets/studybuddy.webp'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <View>
+        <Image source={Logo} style={[styles.image, { width: 100, height: 100 }]} resizeMode="cover" />
+      </View>
+      <Text style={styles.title}>Study Buddy</Text>
+      <Text style={[styles.shadowedText, { marginTop: 10, marginBottom: 5 }]}>
+        Exams
+      </Text>
+      <Text style={[styles.shadowedText, { marginTop: 1, marginBottom: 5 }]}>
+        Practice
+      </Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  shadowedText: {
+    textShadowColor: 'rgba(0, 0, 0, 0.3)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 5,
+  },
+  card: {
+    backgroundColor: '#c4d5daad',
+    borderRadius: 5,
+    padding: 10,
+    boxShadow: 'rgba(0, 0, 0, 0.37)',
+  },
+  image: {
+    width: 5,
+    height: 5,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#00000069',
+  }
+})
